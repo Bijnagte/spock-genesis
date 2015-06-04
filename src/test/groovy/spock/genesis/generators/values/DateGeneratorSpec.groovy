1b3e5bf6ca8b300a311dc9a3812910c0f53ae375
Tom <tom.mclaughlin@gmail.com>
2015-06-04 13:18:21 -0500

    Generating dates failed when a range was specified (.millis vs .time)

---
+++ src/test/groovy/spock/genesis/generators/values/DateGeneratorSpec.groovy
@@ -0,0 +1,22 @@
+package spock.genesis.generators.values
+
+import groovy.time.TimeCategory
+import spock.lang.Specification
+
+class DateGeneratorSpec extends Specification {
+	
+	def 'generate in range'() {
+		setup:
+			final sample = 10
+		when:
+			def generator = new DateGenerator(low, high)
+			def results = generator.take(sample).realized
+		
+		then:
+			results.size() == sample
+			results.every { it <= high && it >= low }
+		where:
+			low | high
+			new Date() | new Date() + 10 
+	}
+}
