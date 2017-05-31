246b1acc99b0ff001fb2e1ab1f09a9a5e2529278
MaPhi Werner <maphi@codecentric.de>
2017-05-31 20:28:43 +0200

    Modified test to increase test coverage of ByteArrayGenerator

--- src/test/groovy/spock/genesis/generators/values/ByteArrayGeneratorSpec.groovy
+++ src/test/groovy/spock/genesis/generators/values/ByteArrayGeneratorSpec.groovy
@@ -7,9 +7,13 @@ class ByteArrayGeneratorSpec extends Specification {
     def 'setting seed produces the same sequences for different generators' () {
         given:
             def a = new ByteArrayGenerator().seed(seed).take(100).realized
-            def b = new ByteArrayGenerator().seed(seed).take(100).realized
+            def b = new ByteArrayGenerator(ByteArrayGenerator.DEFAULT_LENGTH_LIMIT).seed(seed).take(100).realized
+            def c = new ByteArrayGenerator(0, ByteArrayGenerator.DEFAULT_LENGTH_LIMIT).seed(seed).take(100).realized
+            def d = new ByteArrayGenerator((0..ByteArrayGenerator.DEFAULT_LENGTH_LIMIT) as IntRange).seed(seed).take(100).realized
         expect:
             a == b
+            b == c
+            c == d
         where:
             seed << [Long.MIN_VALUE, 100, Long.MAX_VALUE]
     }
